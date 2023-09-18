80432bb77dae5b665afaeef7208c2b6f6071ed45
Mayank9962 <141384161+Mayank9962@users.noreply.github.com>
2023-09-18 09:37:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,108 @@
-let startBtn = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let resetBtn = document.getElementById('reset');
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+//Winning Pattern Array
+let winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+//Player 'X' plays first
+let xTurn = true;
+let count = 0;
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+//Disable All Buttons
+const disableButtons = () => {
+  btnRef.forEach((element) => (element.disabled = true));
+  //enable popup
+  popupRef.classList.remove("hide");
+};
 
-startBtn.addEventListener('click', function () {
-	timer = true;
-	stopWatch();
+//Enable all buttons (For New Game and Restart)
+const enableButtons = () => {
+  btnRef.forEach((element) => {
+    element.innerText = "";
+    element.disabled = false;
+  });
+  //disable popup
+  popupRef.classList.add("hide");
+};
+
+//This function is executed when a player wins
+const winFunction = (letter) => {
+  disableButtons();
+  if (letter == "X") {
+    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+  } else {
+    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+  }
+};
+
+//Function for draw
+const drawFunction = () => {
+  disableButtons();
+  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+};
+
+//New Game
+newgameBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+restartBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
 });
 
-stopBtn.addEventListener('click', function () {
-	timer = false;
+//Win Logic
+const winChecker = () => {
+  //Loop through all win patterns
+  for (let i of winningPattern) {
+    let [element1, element2, element3] = [
+      btnRef[i[0]].innerText,
+      btnRef[i[1]].innerText,
+      btnRef[i[2]].innerText,
+    ];
+    //Check if elements are filled
+    //If 3 empty elements are same and would give win as would
+    if (element1 != "" && (element2 != "") & (element3 != "")) {
+      if (element1 == element2 && element2 == element3) {
+        //If all 3 buttons have same values then pass the value to winFunction
+        winFunction(element1);
+      }
+    }
+  }
+};
+
+//Display X/O on click
+btnRef.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (xTurn) {
+      xTurn = false;
+      //Display X
+      element.innerText = "X";
+      element.disabled = true;
+    } else {
+      xTurn = true;
+      //Display Y
+      element.innerText = "O";
+      element.disabled = true;
+    }
+    //Increment count on each click
+    count += 1;
+    if (count == 9) {
+      drawFunction();
+    }
+    //Check for win on every click
+    winChecker();
+  });
 });
-
-resetBtn.addEventListener('click', function () {
-	timer = false;
-	hour = 0;
-	minute = 0;
-	second = 0;
-	count = 0;
-	document.getElementById('hr').innerHTML = "00";
-	document.getElementById('min').innerHTML = "00";
-	document.getElementById('sec').innerHTML = "00";
-	document.getElementById('count').innerHTML = "00";
-});
-
-function stopWatch() {
-	if (timer) {
-		count++;
-
-		if (count == 100) {
-			second++;
-			count = 0;
-		}
-
-		if (second == 60) {
-			minute++;
-			second = 0;
-		}
-
-		if (minute == 60) {
-			hour++;
-			minute = 0;
-			second = 0;
-		}
-
-		let hrString = hour;
-		let minString = minute;
-		let secString = second;
-		let countString = count;
-
-		if (hour < 10) {
-			hrString = "0" + hrString;
-		}
-
-		if (minute < 10) {
-			minString = "0" + minString;
-		}
-
-		if (second < 10) {
-			secString = "0" + secString;
-		}
-
-		if (count < 10) {
-			countString = "0" + countString;
-		}
-
-		document.getElementById('hr').innerHTML = hrString;
-		document.getElementById('min').innerHTML = minString;
-		document.getElementById('sec').innerHTML = secString;
-		document.getElementById('count').innerHTML = countString;
-		setTimeout(stopWatch, 10);
-	}
-}
+//Enable Buttons and disable popup on page load
+window.onload = enableButtons;
